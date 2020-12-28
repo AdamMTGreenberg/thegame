33eaef3d59156da7d4c2fb63e0ef662096c72149
adamgreenberg <adam.m.t.greenberg@gmail.com>
2020-12-27 22:44:06 -0800

    Dry run of the setup for the basic App initilization

--- app/src/main/java/com/amtgreenberg/thegame/MainApplication.kt
+++ app/src/main/java/com/amtgreenberg/thegame/MainApplication.kt
@@ -1,6 +1,37 @@
 package com.amtgreenberg.thegame
 
 import android.app.Application
+import com.amtgreenberg.thegame.di.component.AppComponent
+import com.amtgreenberg.thegame.di.component.DaggerAppComponent
+import com.facebook.stetho.Stetho
+import timber.log.Timber
 
 class MainApplication : Application() {
+
+    companion object {
+        lateinit var appComponent: AppComponent
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = initDagger(this)
+        initStetho()
+        initTimber()
+//        InternetUtil.init(this)
+    }
+
+    private fun initDagger(mainApplication: MainApplication): AppComponent {
+        return DaggerAppComponent.builder()
+                .build()
+    }
+
+    private fun initStetho() {
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+        }
+    }
+
+    private fun initTimber() {
+        if (BuildConfig.DEBUG) { Timber.plant(Timber.DebugTree()) }
+    }
 }
