5536ce8417d50483b5700f92b7f2b05dc90da214
adamgreenberg <adam.m.t.greenberg@gmail.com>
2020-12-27 23:17:42 -0800

    Adding the app module

--- app/src/main/java/com/amtgreenberg/thegame/di/component/AppComponent.kt
+++ app/src/main/java/com/amtgreenberg/thegame/di/component/AppComponent.kt
@@ -1,8 +1,13 @@
 package com.amtgreenberg.thegame.di.component
 
+import com.amtgreenberg.thegame.di.module.AppModule
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component
+@Component(
+    modules = [
+        AppModule::class
+    ]
+)
 interface AppComponent
