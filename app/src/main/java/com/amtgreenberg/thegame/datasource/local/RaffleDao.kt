50c7c79eda30b4c2716adc30aee0aae4de1d57db
adamgreenberg <adam.m.t.greenberg@gmail.com>
2020-12-29 00:04:16 -0800

    Creating the DAO for inserting and accessing the Raffle DB

--- app/src/main/java/com/amtgreenberg/thegame/datasource/local/RaffleDao.kt
+++ app/src/main/java/com/amtgreenberg/thegame/datasource/local/RaffleDao.kt
@@ -1,7 +1,47 @@
 package com.amtgreenberg.thegame.datasource.local
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.amtgreenberg.thegame.model.Entry
+import com.amtgreenberg.thegame.model.Participant
+import com.amtgreenberg.thegame.model.ParticipantEntries
 
 @Dao
 interface RaffleDao {
+
+    /**********************************************************
+     * QUERIES
+     ***********************************************************/
+
+    @Query("SELECT * FROM Participant")
+    suspend fun getParticipants(): List<Participant>
+
+    @Query("SELECT * FROM Entry")
+    suspend fun getEntries(): List<Entry>
+
+    @Query("SELECT * FROM Participant WHERE name IS :name")
+    suspend fun getParticipantEntries(name: String): List<ParticipantEntries>
+
+    @Query("SELECT * FROM Entry WHERE Entry.participant_name IS :name AND Entry.is_valid = 1")
+    suspend fun getActiveParticipantEntries(name: String): List<Entry>
+
+    /**********************************************************
+     * INSERTS
+     ***********************************************************/
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertParticipant(vararg participant: Participant)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertEntry(vararg entry: Entry)
+
+    /**********************************************************
+     * UPDATES
+     ***********************************************************/
+
+    @Update
+    fun updateEntry(vararg entry: Entry)
 }
