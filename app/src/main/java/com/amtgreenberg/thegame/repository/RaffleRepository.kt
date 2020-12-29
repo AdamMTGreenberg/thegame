f7f94c9a5a0798e29731041881acd2d7c5c759be
adamgreenberg <adam.m.t.greenberg@gmail.com>
2020-12-29 00:25:22 -0800

    Create the interface defining the raffle repository

---
+++ app/src/main/java/com/amtgreenberg/thegame/repository/RaffleRepository.kt
@@ -0,0 +1,20 @@
+package com.amtgreenberg.thegame.repository
+
+import com.amtgreenberg.thegame.data.ResultData
+import com.amtgreenberg.thegame.model.Entry
+import com.amtgreenberg.thegame.model.Participant
+
+/**
+ * Repository which manages the raffle entries
+ */
+interface RaffleRepository {
+
+    suspend fun addEntry(participant: Participant): ResultData<Entry>
+    suspend fun addParticipant(name: Participant): ResultData<Entry>
+    suspend fun addParticipantEntries(
+        participant: Participant,
+        entryIds: List<Int>
+    ): ResultData<List<Entry>>
+
+    suspend fun obtainRaffleWinner(shouldFinalizeAfterSelection: Boolean = true): ResultData<Entry>
+}
