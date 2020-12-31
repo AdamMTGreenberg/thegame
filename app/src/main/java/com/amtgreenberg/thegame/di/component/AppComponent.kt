fac4325c05e99eab8bb05aafb2e4ed1efbeaf37d
adamgreenberg <adam.m.t.greenberg@gmail.com>
2020-12-30 22:56:30 -0800

    Register the repository and the local storage module in the the app component

--- app/src/main/java/com/amtgreenberg/thegame/di/component/AppComponent.kt
+++ app/src/main/java/com/amtgreenberg/thegame/di/component/AppComponent.kt
@@ -1,13 +1,17 @@
 package com.amtgreenberg.thegame.di.component
 
 import com.amtgreenberg.thegame.di.module.AppModule
+import com.amtgreenberg.thegame.di.module.LocalStorageModule
+import com.amtgreenberg.thegame.di.module.RepositoryModule
 import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
 @Component(
     modules = [
-        AppModule::class
+        AppModule::class,
+        RepositoryModule::class,
+        LocalStorageModule::class
     ]
 )
 interface AppComponent
