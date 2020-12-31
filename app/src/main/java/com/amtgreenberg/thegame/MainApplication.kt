00a6a640b185dc7a6560cb5b1ad367b3f710d058
adamgreenberg <adam.m.t.greenberg@gmail.com>
2020-12-30 23:41:19 -0800

    Propperly init the dagger graph

--- app/src/main/java/com/amtgreenberg/thegame/MainApplication.kt
+++ app/src/main/java/com/amtgreenberg/thegame/MainApplication.kt
@@ -3,6 +3,8 @@ package com.amtgreenberg.thegame
 import android.app.Application
 import com.amtgreenberg.thegame.di.component.AppComponent
 import com.amtgreenberg.thegame.di.component.DaggerAppComponent
+import com.amtgreenberg.thegame.di.module.AppModule
+import com.amtgreenberg.thegame.di.module.LocalStorageModule
 import com.facebook.stetho.Stetho
 import timber.log.Timber
 
@@ -21,7 +23,9 @@ class MainApplication : Application() {
 
     private fun initDagger(mainApplication: MainApplication): AppComponent {
         return DaggerAppComponent.builder()
-                .build()
+            .appModule(AppModule(application = mainApplication))
+            .localStorageModule(LocalStorageModule(application = mainApplication))
+            .build()
     }
 
     private fun initStetho() {
